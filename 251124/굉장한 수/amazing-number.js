fd729e040642005d64a4801bcb213b97f32817c4
FatBuu90 <jbgong90@gmail.com>
2025-11-24 21:01:48 +0900

    Title: 굉장한 수, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251124/굉장한 수/amazing-number.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim());
+
+if ( input % 2 == 1 && input % 3 == 0 ) {
+    console.log("true");
+} else if ( input % 2 == 0 && input % 5 == 0 ) {
+    console.log("true");
+} else {
+    console.log("false");
+}
